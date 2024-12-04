f03857af0ac3785045146801c4d85d5f238c914a
IvoFer9 <2212532@my.ipleiria.pt>
2024-12-04 12:39:08 +0000

    SensorBean CRUD basico

--- PMEI-backend/src/main/java/pt/ipleiria/estg/ei/dae/projeto/projetopmei/ejbs/SensorBean.java
+++ PMEI-backend/src/main/java/pt/ipleiria/estg/ei/dae/projeto/projetopmei/ejbs/SensorBean.java
@@ -8,6 +8,7 @@ import pt.ipleiria.estg.ei.dae.projeto.projetopmei.entities.entityTypes.SensorTy
 import pt.ipleiria.estg.ei.dae.projeto.projetopmei.entities.entityTypes.StatusType;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class SensorBean {
 
@@ -26,5 +27,43 @@ public class SensorBean {
         } catch (Exception e) {
             throw new Exception(e);
         }
+    }
+
+    // Encontra por ID
+    public Sensor find(long sensorId) throws Exception {
+        Sensor sensor = entityManager.find(Sensor.class, sensorId);
+        if (sensor == null) {
+            throw new Exception("Sensor with ID " + sensorId + " not found");
+        }
+        return sensor;
+    }
+
+    // Encontra TODOS os sensores
+    public List<Sensor> findAll(){
+        return entityManager.createQuery("select s from Sensor s").getResultList();
+    }
+
+    // Update de um sensor
+    public void update(long sensorId, SensorType sensorType, StatusType statusType, LocalDateTime timestamp, String currentValue)
+            throws Exception {
+        Sensor sensor = find(sensorId);
+
+        sensor.setSensorType(sensorType);
+        sensor.setStatus(statusType);
+        sensor.setTimestamp(timestamp);
+        sensor.setCurrentValue(currentValue);
+
+        try {
+            entityManager.merge(sensor);
+        } catch (Exception e) {
+            throw new Exception(e);
+        }
+    }
+
+    // Delete sensor
+    public void delete(long sensorId)
+            throws Exception {
+        Sensor sensor = find(sensorId);
+        entityManager.remove(sensor);
     }
 }
